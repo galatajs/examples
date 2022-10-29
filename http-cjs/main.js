6f3673ba99cbda1f7a08b732d0a26d6b9f968057
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2022-10-29 16:36:44 +0300

    feat(#2): add example http and commonjs

---
+++ http-cjs/main.js
@@ -0,0 +1,38 @@
+const { createApp } = require("@istanbul/app");
+const { createHttpServer, createRouter } = require("@istanbul/http");
+
+const app = createApp();
+const server = createHttpServer();
+app.register(server);
+
+const router = createRouter({ prefix: "blog" });
+
+const posts = [];
+
+router.get("", (req, res) => {
+  res.successData("Posts fetched successfully", posts);
+});
+
+router.get(":id", (req, res) => {
+  const post = posts.find((post) => post.id === +req.params.id);
+  if (post) {
+    res.successData("Post fetched successfully", post);
+  } else {
+    res.notFound("Post not found");
+  }
+});
+
+router.post("", (req, res) => {
+  const post = {
+    id: Math.trunc(Math.random() * 1000),
+    title: req.body.title,
+  };
+  posts.push(post);
+  res.successData("Post created successfully", post);
+});
+
+app.start().then(() => {
+  console.log(
+    `Server started on http://${server.config.host}:${server.config.port}`
+  );
+});
